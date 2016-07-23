adba63cc8b919570a3fc12850f7f20a84606f65d
Arun <AK@ARUNs-MacBook-Pro.local>
2016-07-23 18:26:54 -0400

    Initial Commit

---
+++ src/main/java/com/ydsworld/ExpensesApplication.java
@@ -0,0 +1,12 @@
+package com.ydsworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesApplication.class, args);
+	}
+}
