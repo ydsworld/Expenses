ad4f5a21795ca9d6f8f94bd13bacdf1c3722db1f
Arun <AK@ARUNs-MacBook-Pro.local>
2016-07-23 23:48:46 -0400

    change controller annotation

--- src/main/java/com/ydsworld/controller/HomeController.java
+++ src/main/java/com/ydsworld/controller/HomeController.java
@@ -1,18 +1,18 @@
 package com.ydsworld.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class HomeController {
 
     @RequestMapping("/home")
-    public String home(){
-
+    public String home(Model model) {
+        model.addAttribute("messsage", "Controller");
         return "home";
-
     }
 
 
