8abb1d8acea9e544e3e1f1d6e5ca22d73d8b4dcc
kinpatel <kgpatel7@gmail.com>
2016-07-30 19:26:39 -0400

    adding git ignore

--- src/main/java/com/ydsworld/config/MvcConfig.java
+++ src/main/java/com/ydsworld/config/MvcConfig.java
@@ -1,4 +1,4 @@
-package com.ydsworld;
+package com.ydsworld.config;
 
 
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/ydsworld/config/WebSecurityConfig.java
+++ src/main/java/com/ydsworld/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.ydsworld;
+package com.ydsworld.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
